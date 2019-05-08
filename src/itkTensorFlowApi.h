932fff939e0a12544adcc90a3054f78ffa75811e
Konrad Werys <konradwerys2@gmail.com>
2019-05-08 19:32:38 +0100

    feat: introducing itkTensorFlowApi v7

--- src/itkTensorFlowApi.h
+++ src/itkTensorFlowApi.h
@@ -20,10 +20,10 @@ namespace itk {
     template class ITKTENSORFLOWLIB_EXPORT Image<float, 3>;
     template class ITKTENSORFLOWLIB_EXPORT Image<double, 3>;
 
-    template class ITKTENSORFLOWLIB_EXPORT TensorFlowImageFilter<Image<short, 3>, Image<short, 3> >;
-    template class ITKTENSORFLOWLIB_EXPORT TensorFlowImageFilter<Image<int, 3>, Image<int, 3> >;
-    template class ITKTENSORFLOWLIB_EXPORT TensorFlowImageFilter<Image<float, 3>, Image<float, 3> >;
-    template class ITKTENSORFLOWLIB_EXPORT TensorFlowImageFilter<Image<double, 3>, Image<double, 3> >;
+    template class ITKTENSORFLOWLIB_EXPORT TensorFlowImageFilter<typename Image<short, 3>, typename Image<short, 3> >;
+    template class ITKTENSORFLOWLIB_EXPORT TensorFlowImageFilter<typename Image<int, 3>, typename Image<int, 3> >;
+    template class ITKTENSORFLOWLIB_EXPORT TensorFlowImageFilter<typename Image<float, 3>, typename Image<float, 3> >;
+    template class ITKTENSORFLOWLIB_EXPORT TensorFlowImageFilter<typename Image<double, 3>, typename Image<double, 3> >;
 }
 
 #endif //ITKTENSORFLOW_ITKTENSORFLOWAPI_H
